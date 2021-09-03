f7daac8c371968333cacb94722415fafe4f1c0b7
gagz092-mb <mikhaelhamoy@gmail.com>
2021-09-03 10:34:26 -0400

    implement front end

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
@@ -118,4 +120,20 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
